01c37da8e08418228909e67eed672fbbf8968824
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-29 23:13:33 +0600

    Fri Mar 29 11:13:33 PM +06 2024

--- src/main.sh
+++ src/main.sh
@@ -1,10 +1,12 @@
-for ((i = 0; i < 10; i++)); do
+it=$1
+for ((i = 1; i <= $it; i++)); do
 	figlet $i | lolcat
 	n=$(tput cols)
 	for ((j = 0; j < n; j++)); do
 		printf "="
 	done
 	printf "\n"
+	cowsay "Ok, now I'm going to commit and push $i"
 	date >>../res/log.txt
 	git add ../res/log.txt
 	git commit -m "log $i"
